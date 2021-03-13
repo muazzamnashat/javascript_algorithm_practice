dc335976d5af83cd7f9adcef727d123068315629
muazzamnashat <muazzamhc.nashat@gmail.com>
2021-03-13 08:12:16 -0500

    lint code

--- selection_sort.js
+++ selection_sort.js
@@ -1,17 +1,17 @@
 // implement selection sort in JS
 
 const selectionSort = (arr) => {
-    const swap = (array, idx1 ,idx2 ) => {
-        [array[idx1], array[idx2]] = [array[idx2], array[idx1]]
+  const swap = (array, idx1, idx2) => {
+    [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
+  };
+  for (let i = 0; i < arr.length; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIdx]) minIdx = j;
     }
-    for (let i = 0; i < arr.length; i++){
-        let minIdx = i; 
-        for (let j = i+1; j < arr.length; j++){
-            if ( arr[j] < arr[minIdx]) minIdx = j;
-        }
-        if ( minIdx !== i) swap(arr, minIdx, i)
-    }
-    return arr;
-}
+    if (minIdx !== i) swap(arr, minIdx, i);
+  }
+  return arr;
+};
 
-console.log(selectionSort([1,4,2,7,9,5]))
+console.log(selectionSort([1, 4, 2, 7, 9, 5]));
